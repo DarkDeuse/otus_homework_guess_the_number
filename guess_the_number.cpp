e91cdb6f826788ef2b161cb8be94a15c824d4bc2
DarkDeuse <sasha12345678_92@mail.ru>
2023-03-06 02:25:19 +0300

    Изменена логика работы с заданным лимитом в ручную, теперь выдает ошибку No Max Limit, так же исправдена ошибка запуска программы после вызова -table

--- guess_the_number.cpp
+++ guess_the_number.cpp
@@ -9,18 +9,22 @@
 int main(int argc, char** argv) {
 	std::string high_scores_filename = "high_scores.txt";
 	int upper_limit = 0;
+	std::string arg1_value{ argv[1] };
 	if (argc == 3){
-		std::string arg1_value{ argv[1] };
 		if (arg1_value == "-max"){
 			upper_limit = std::stoi(argv[2]);
 		}
+	}
 		else if (argc == 2){
 		if (arg1_value == "-table"){
 			read_the_file(high_scores_filename);
 			return 0;
 		}
+		else{
+			std::cout << "No Max Limit!" << std::endl;
+			return 1;
 		}
-	}
+		}
 	int riddled_number = imagine_number(upper_limit);
 	std::cout << "Hi! Enter your name, please:" << std::endl;
 	std::string user_name;
@@ -29,5 +33,6 @@ int main(int argc, char** argv) {
 	write_in_file(score, user_name, high_scores_filename);
 	read_the_file(high_scores_filename);
 	
-	return 0;	
-}
+	return 0;
+	}
+
