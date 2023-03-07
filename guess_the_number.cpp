4c2450a9773bc9bacad9a20a042583c75264bbae
Александр <sasha12345678_92@mail.ru>
2023-03-07 16:23:40 +0300

    Изменён текст ошибки в соответсвии с рекомендацией, проверен вызов без параметров, с параметром max без числа и с числом, table, все корректно работает.

--- guess_the_number.cpp
+++ guess_the_number.cpp
@@ -9,22 +9,21 @@
 int main(int argc, char** argv) {
 	std::string high_scores_filename = "high_scores.txt";
 	int upper_limit = 0;
+	if (argc >= 2) {
 	std::string arg1_value{ argv[1] };
-	if (argc == 3){
-		if (arg1_value == "-max"){
-			upper_limit = std::stoi(argv[2]);
-		}
+	if (arg1_value == "-max"){
+	if (argc < 3) {
+	std::cout << "Wrong usage! The argument '-max' requires some value!" << std::endl;
+	return -1;
 	}
-		else if (argc == 2){
-		if (arg1_value == "-table"){
+	
+	upper_limit = std::stoi(argv[2]);
+	}
+	if (arg1_value == "-table"){
 			read_the_file(high_scores_filename);
 			return 0;
-		}
-		else{
-			std::cout << "No Max Limit!" << std::endl;
-			return 1;
-		}
-		}
+	}
+	}
 	int riddled_number = imagine_number(upper_limit);
 	std::cout << "Hi! Enter your name, please:" << std::endl;
 	std::string user_name;
@@ -34,5 +33,6 @@ int main(int argc, char** argv) {
 	read_the_file(high_scores_filename);
 	
 	return 0;
-	}
+	
+}
 
